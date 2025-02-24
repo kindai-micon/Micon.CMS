ba6eb29b23f928f05866d89013fdb517085df31c
actbit <57023457+actbit@users.noreply.github.com>
2025-02-24 21:27:18 +0900

    [add] ComponentRelation

--- Micon.CMS/Models/ComponentRelation.cs
+++ Micon.CMS/Models/ComponentRelation.cs
@@ -1,6 +1,15 @@
-﻿namespace Micon.CMS.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Micon.CMS.Models
 {
     public class ComponentRelation
     {
+        [ForeignKey(nameof(Parent))]
+        public Guid ParentId { get; set; }
+        public Component Parent { get; set; }
+        [ForeignKey(nameof(Child))]
+        public Guid ChildId { get; set; }
+        public Component Child { get; set; }
+        public int order { get; set; }
     }
 }
