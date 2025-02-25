8a2d8f269cc19ae60931b95a8ed8e4756328d858
actbit <57023457+actbit@users.noreply.github.com>
2025-02-25 09:14:05 +0900

    [add] DbSet

---
+++ Micon.CMS/ApplicationDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Micon.CMS.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography.Pkcs;
+
+namespace Micon.CMS
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Tenant> Tenants { get; set; }
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+        public DbSet<ApplicationRole> ApplicationRoles { get; set; }
+        public DbSet<Page> Pages { get; set; }
+        public DbSet<PageCategory> PageCategories { get; set; }
+        public DbSet<PageHistory> PageHistories { get; set; }
+        public DbSet<PageTemplate> PageTemplates { get; set; }
+        public DbSet<PageTemplateHistory> PageTemplateHistories { get; set; }
+        public DbSet<Component> Components { get; set; }
+        public DbSet<ComponentRelation> ComponentRelations { get; set; }
+        public DbSet<ComponentSetting> ComponentSettings { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
