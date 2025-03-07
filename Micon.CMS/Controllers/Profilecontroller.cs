fa3e1ef27b1707344594e237d4b59b441e17da33
taka709113 <taka713taka@gmail.com>
2025-03-07 16:57:44 +0900

    プロフィールのベースを作りました

---
+++ Micon.CMS/Controllers/Profilecontroller.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Micon.CMS.Controllers
+{
+    public class Profilecontroller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
