ba327a3a7957885758e326eac58f9a2288a86f9f
Stungunis5tr0ng <stungunis5tr0ng@gmail.com>
2025-03-09 11:57:03 +0900

    [add]ログインモデル [edit]ログインコントローラ,ログイン画面

--- Micon.CMS/Controllers/LoginController.cs
+++ Micon.CMS/Controllers/LoginController.cs
@@ -1,12 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Micon.CMS.Models;
+using Micon.CMS.Models.Form;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Micon.CMS.Controllers
 {
-    public class LoginController : Controller
+    public class LoginController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager) : Controller
     {
         public IActionResult Index()
         {
             return View();
+        }
+        [ValidateAntiForgeryToken]
+        [HttpPost]
+        public async Task<IActionResult> Login([Bind("Organization, UserName, Password")] LoginModel model)
+        {
+           var SignInResult = await signInManager.PasswordSignInAsync(model.UserName, model.Password, true, false);
+            if(SignInResult.Succeeded)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                return View("Index");
+            }
         }
     }
 }
