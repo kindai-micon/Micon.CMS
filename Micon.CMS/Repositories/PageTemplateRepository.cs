ff5e7fa0a38ee18c617b6ffd4ed200233c533f3b
actbit <57023457+actbit@users.noreply.github.com>
2025-03-13 18:15:54 +0900

    [fix] get tree

--- Micon.CMS/Repositories/PageTemplateRepository.cs
+++ Micon.CMS/Repositories/PageTemplateRepository.cs
@@ -8,9 +8,54 @@ namespace Micon.CMS.Repositories
 
         public Task<List<PageCategory>> GetPageCategoriesAsync(PageTemplate pageTemplate, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            
         }
 
+        public async Task<List<ComponentHierarchy>> GetAllChildComponentsFromPageTemplateWithCTE(PageTemplate pageTemplate)
+        {
+            // 再帰的CTEを使用してすべての子コンポーネントを取得
+            var components = await dbContext.Set<ComponentHierarchy>()
+                .FromSqlRaw(_componentHierarchyQuery, pageTemplate.Id)
+                .ToListAsync();
 
+            return components;
+        }
+
+        const string _componentHierarchyQuery = @"
+        WITH RECURSIVE ComponentHierarchy AS (
+            SELECT 
+                cr.""Id"",
+                cr.""ParentId"",
+                cr.""ChildId"",
+                cr.""Order"",
+                1 AS Level,
+                p.""Id"" AS ParentComponentId,
+                p.""Name"" AS ParentComponentName,
+                c.""Id"" AS ChildComponentId,
+                c.""Name"" AS ChildComponentName
+            FROM ""ComponentRelation"" cr
+            LEFT JOIN ""Component"" c ON c.""Id"" = cr.""ChildId""
+            WHERE cr.""Id"" = (SELECT ""ComponentRelationId"" FROM ""PageTemplate"" WHERE ""Id"" = {0})
+
+            UNION ALL
+
+            SELECT 
+                cr.""Id"",
+                cr.""ParentId"",
+                cr.""ChildId"",
+                cr.""Order"",
+                ch.Level + 1 AS Level,
+                p.""Id"" AS ParentComponentId,
+                p.""Name"" AS ParentComponentName,
+                c.""Id"" AS ChildComponentId,
+                c.""Name"" AS ChildComponentName
+            FROM ""ComponentRelation"" cr
+            INNER JOIN ComponentHierarchy ch ON cr.""ParentId"" = ch.""ChildId""
+            LEFT JOIN ""Component"" p ON p.""Id"" = cr.""ParentId""
+            LEFT JOIN ""Component"" c ON c.""Id"" = cr.""ChildId""
+        )
+
+        SELECT * FROM ComponentHierarchy;
+            ";
     }
 }
