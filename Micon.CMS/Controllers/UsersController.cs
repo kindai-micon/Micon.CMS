5f22d36473dbda76ae1ac13914a46a736b622e58
katiware <zhongjiashengwu@gmail.com>
2025-03-30 13:39:31 +0900

    [add]モデルを追加

--- Micon.CMS/Controllers/UsersController.cs
+++ Micon.CMS/Controllers/UsersController.cs
@@ -1,4 +1,7 @@
-﻿namespace Micon.CMS.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using Micon.CMS.Models;
+using Micon.CMS.Models.Form;
+namespace Micon.CMS.Controllers
 {
     public class UsersController
     {
