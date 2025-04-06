05c0a424c87bc56f548bd1dbfa29ffcb3679dc49
katiware <zhongjiashengwu@gmail.com>
2025-04-06 11:17:37 +0900

    [change]UsersController

--- Micon.CMS/Controllers/UsersController.cs
+++ Micon.CMS/Controllers/UsersController.cs
@@ -3,10 +3,16 @@ using Micon.CMS.Models;
 using Micon.CMS.Models.Form;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Micon.CMS.Controllers
 {
-    public class UsersController
+    public class UsersController(UserManager<ApplicationUser> userManager):Controller
     {
+        public async Task<IActionResult>Index()
+        {
+            var userlist = await userManager.Users.ToListAsync();
+            return View(userlist);
+        }
     }
 }
