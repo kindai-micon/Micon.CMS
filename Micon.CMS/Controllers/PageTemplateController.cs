4af6391b4c87fa58d3ee9f866d9680b51186becb
actbit <57023457+actbit@users.noreply.github.com>
2025-04-06 10:59:04 +0900

    [update] page template view

--- Micon.CMS/Controllers/PageTemplateController.cs
+++ Micon.CMS/Controllers/PageTemplateController.cs
@@ -1,11 +1,43 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Micon.CMS.Models;
+using Micon.CMS.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis;
+using System.Threading.Tasks;
 
 namespace Micon.CMS.Controllers
 {
-    public class PageTemplateController : Controller
+    public class PageTemplateController(IPageTemplateRepository pageTemplateRepository) : Controller
     {
-        public IActionResult Index()
+        public async Task<IActionResult> Index(CancellationToken cancellation)
         {
+            var templates = await pageTemplateRepository.GetAllWithCategoryAsync(cancellation);
+            return View(templates);
+        }
+
+        public async Task<IActionResult> Create( CancellationToken cancellationToken)
+        {
+
+            var category = new PageCategory();
+            var template = new PageTemplate();
+            template.PageCategoryId = category.Id;
+            template.PageCategory = category;
+            return View(template);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateSubmit(PageTemplate pageTemplate,CancellationToken cancellationToken)
+        {
+            var newPageTemplate = await pageTemplateRepository.CreateAsync(pageTemplate, cancellationToken);
+
+            return RedirectToAction(nameof(Index), nameof(PageTemplateController));
+        }
+        public IActionResult Edit(Guid Id, CancellationToken cancellationToken)
+        {
+            var pageTemplate = pageTemplateRepository.GetByIdAsync(Id, cancellationToken);
+            if (pageTemplate == null)
+            {
+                return RedirectToAction(nameof(Index), nameof(PageTemplateController));
+            }
             return View();
         }
     }
