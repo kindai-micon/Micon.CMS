5579cc3085a7b841b6350c5a490a7a93dc63d336
katiware <zhongjiashengwu@gmail.com>
2025-04-06 12:45:47 +0900

    [add]コントローラーの追加 　　ビューの追加        モデルの追加

---
+++ Micon.CMS/Controllers/UserDeleteController.cs
@@ -0,0 +1,31 @@
+﻿using Micon.CMS.Models;
+using Micon.CMS.Models.Form;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Micon.CMS.Controllers
+{
+    public class UserDeleteController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager) : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [ValidateAntiForgeryToken]
+        [HttpPost]
+        public async Task<IActionResult> Delete([Bind("Password")] LoginModel model)
+        {
+            var user = await userManager.GetUserAsync(User);
+            var passward_ok = await userManager.CheckPasswordAsync(user,model.Password);
+            if (passward_ok)
+            {
+
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                return View("Index");
+            }
+        }
+    }
+}
---
+++ Micon.CMS/Models/Form/UserDeleteModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Micon.CMS.Models.Form
+{
+    public class UserDeleteModel
+    {
+        [Required]
+        public string Password { get; set; }
+    }
+}
