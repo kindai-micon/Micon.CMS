81f7041ade5b25c177a26ee041f3c7fd697bbcb0
taka709113 <taka713taka@gmail.com>
2025-04-06 11:58:47 +0900

    ページの一覧についてつくりました

---
+++ Micon.CMS/Controllers/PageListController.cs
@@ -0,0 +1,25 @@
+﻿using Micon.CMS.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Micon.CMS.Controllers
+{
+    public class PageListController : Controller
+    {
+        public class ProductController : Controller
+        {
+            private readonly List<PageTemplate> _products = new List<PageTemplate>
+    {
+        new PageTemplate { Name = ,PageCategories = , Pages =  ,PageTemplateHistories = },
+        new PageTemplate { Id = 2, Name = "Product2", Price = 200 },
+        new PageTemplate { Id = 3, Name = "Product3", Price = 300 }
+    };
+          
+
+
+            public IActionResult Index()
+            {
+                return View();
+            }
+        }
+    }
+}
