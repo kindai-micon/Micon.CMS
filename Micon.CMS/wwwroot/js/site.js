b32117cdff7a2e39ee2697889ba900ed7200034f
mamenz752 <mamenz752@gmail.com>
2025-07-01 13:30:40 +0900

    wip: fix aside menu

--- Micon.CMS/wwwroot/js/site.js
+++ Micon.CMS/wwwroot/js/site.js
@@ -4,23 +4,23 @@
 // Write your JavaScript code.
 
 // move active class on menu 
-document.addEventListener("DOMContentLoaded", function () {
-    const listItems = document.querySelectorAll(".aside-menu li");
+//document.addEventListener("DOMContentLoaded", function () {
+//    const listItems = document.querySelectorAll(".aside-menu li");
 
-    listItems.forEach(item => {
-        item.addEventListener("click", function (e) {
-            e.preventDefault(); // aタグなどのデフォルト動作を無効化
+//    listItems.forEach(item => {
+//        item.addEventListener("click", function (e) {
+//            e.preventDefault(); // aタグなどのデフォルト動作を無効化
 
-            // クリックされた要素の中で最も近いli要素を取得
-            const clickedLi = e.target.closest("li");
+//            // クリックされた要素の中で最も近いli要素を取得
+//            const clickedLi = e.target.closest("li");
 
-            if (!clickedLi) return;
+//            if (!clickedLi) return;
 
-            // 現在のactiveを削除
-            document.querySelector(".aside-menu .active")?.classList.remove("active");
+//            // 現在のactiveを削除
+//            document.querySelector(".aside-menu .active")?.classList.remove("active");
 
-            // クリックされた要素にactiveを追加
-            clickedLi.classList.add("active");
-        });
-    });
-});
+//            // クリックされた要素にactiveを追加
+//            clickedLi.classList.add("active");
+//        });
+//    });
+//});
