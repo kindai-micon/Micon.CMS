ec623f57d9a4cc4edc6da8bd7c5e35cb10394b17
MIRA_PC\jianr <jianrenbeili52@gmail.com>
2025-08-15 21:56:24 +0900

    GetSettingを実装中

---
+++ Micon.CMS/Repositories/ComponentRepository.cs
@@ -0,0 +1,15 @@
+﻿using Micon.CMS.Models;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+namespace Micon.CMS.Repositories
+{
+    public class ComponentRepository(ApplicationDbContext dbContext) : BaseRepository<Component>(dbContext), IComponentRelationRepository
+    {
+        public override Component GetSetting(ComponentSetting page, ComponentSetting component, ComponentSetting key)
+        {
+            return dbContext.ComponentSettings.
+                Where(x => x.Component.Id == component.ComponentId && x.PageId == page.PageId).
+                ToList();
+        }
+    }
+}
