8771cf16966ecf17aa3a4b677ff3fd8ad4ab0628
MIRA_PC\jianr <jianrenbeili52@gmail.com>
2025-08-15 23:43:44 +0900

    GetSetting, UpdateSettig, CreateSettingを実装、IComponentSettingを作成

--- Micon.CMS/Repositories/ComponentRepository.cs
+++ Micon.CMS/Repositories/ComponentRepository.cs
@@ -1,15 +1,37 @@
 ﻿using Micon.CMS.Models;
-using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
+
 namespace Micon.CMS.Repositories
 {
-    public class ComponentRepository(ApplicationDbContext dbContext) : BaseRepository<Component>(dbContext), IComponentRelationRepository
+    public class ComponentRepository(ApplicationDbContext dbContext) : BaseRepository<Component>(dbContext)
     {
-        public override Component GetSetting(ComponentSetting page, ComponentSetting component, ComponentSetting key)
+        public async Task<ComponentSetting?> GetSettingAsync(Page page, Component component, string key, CancellationToken cancellationToken)
         {
-            return dbContext.ComponentSettings.
-                Where(x => x.Component.Id == component.ComponentId && x.PageId == page.PageId).
-                ToList();
+            return await dbContext.ComponentSettings
+                .Where(x => x.ComponentId == component.Id && x.PageId == page.Id)
+                .FirstOrDefaultAsync(cancellationToken);
+        }
+        public async Task<List<ComponentSetting>> GetSettingsAsync(Page page, Component component, CancellationToken cancellationToken)
+        {
+            return await dbContext.ComponentSettings
+                .Where(x => x.ComponentId == component.Id && x.PageId == page.Id)
+                .ToListAsync(cancellationToken);
+
+        }
+        public void UpdateSetting(ComponentSetting componentSetting)
+        {
+            dbContext.Update(componentSetting);
+            dbContext.SaveChangesAsync();
+        }
+        public async Task<Microsoft.EntityFrameworkCore.ChangeTracking.EntityEntry<Micon.CMS.Models.ComponentSetting>> CreateSetting(Page page, Component component, string key, CancellationToken cancellationToken)
+        {
+            ComponentSetting componentSetting = new ComponentSetting();
+            componentSetting.Key = key;
+            componentSetting.Page = page;
+            componentSetting.PageId = page.Id;
+            componentSetting.Component = component;
+            componentSetting.ComponentId = component.Id;
+            return await dbContext.AddAsync(componentSetting, cancellationToken);
         }
     }
 }
---
+++ Micon.CMS/Repositories/IComponentRepository.cs
@@ -0,0 +1,14 @@
+﻿using Micon.CMS.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Micon.CMS.Repositories
+{
+    public interface IComponentRepository : IBaseRepository<Component>
+    {
+        public Task<ComponentSetting?> GetSettingAsync(Page page, Component component, string key, CancellationToken cancellationToken);
+        public Task<List<ComponentSetting>> GetSettingsAsync(Page page, Component component, CancellationToken cancellationToken);
+        public Task UpdateSettingAsync(ComponentSetting componentSetting, CancellationToken cancellationToken);
+
+    }
+}
+
